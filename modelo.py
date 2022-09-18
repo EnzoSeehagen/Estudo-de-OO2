e8dd90a1e2a8b08066979df8bfdda0a3f9a56045
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-09-18 19:43:26 -0300

    Aplicando herança na classe Playlist

--- modelo.py
+++ modelo.py
@@ -19,26 +19,41 @@ class Programa:
         def nome(self, novo_nome):
             self._nome = novo_nome.title()
 
+        def __str__(self):
+            return f" {self._nome} - {self.ano} : {self._like} Likes"
+
 class Filme(Programa):
 
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f" {self._nome} - {self.ano} - {self.duracao} Minutos: {self._like} Likes"
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f" {self._nome} - {self.ano} - {self.temporadas} Temporadas: {self._like} Likes"
 
-vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(f" {vingadores._nome} - {vingadores.duracao} : {vingadores._like}")
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        super().__init__(programas)
 
+vingadores = Filme('vingadores', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-print(f'{atlanta._nome} - {atlanta.ano} : {atlanta.temporadas}')
+got = Serie("Game of Thrones", 2013, 8)
+bat = Filme("Batman", 2008, 140)
+lor = Filme("Lord of Rings", 2004, 240)
 
-filmes_e_series = [vingadores, atlanta]
 
-for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa, "duracao" ) else programa.temporadas
-    print(f"{programa._nome} -{detalhes} - {programa._like}")
+filmes_e_series = [vingadores, atlanta, got, bat, lor ]
+
+playlist_do_fim_de_semana = Playlist("fim de semana", filmes_e_series)
+
+for programa in playlist_do_fim_de_semana.programas:
+    print(programa)
